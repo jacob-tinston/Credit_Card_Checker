4439fe842233ef419dc6b6a9a18ecd18c26f1eb4
Jacob Tinston <77517237+jacob-tinston@users.noreply.github.com>
2021-02-17 12:51:08 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-const myCard = []
+const myCard = []  // Insert your individual card numbers as separate elements in the arr (eg, [1, 2, 3, 4, 5])
 
-// All valid credit card numbers
+// All valid credit card numbers:
 const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8];
 const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9];
 const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6];
 const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5];
 const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6];
 
-// All invalid credit card numbers
+// All invalid credit card numbers:
 const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5];
 const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3];
 const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4];
 const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5];
 const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4];
 
-// Can be either valid or invalid
+// Can be either valid or invalid:
 const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4];
 const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9];
 const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3];
 const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3];
 const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
 
-// An array of all the arrays above
+// An array of all the arrays above:
 const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5]; 
 
+// Luhn algorithm - checks if a credit card number is valid
 const validateCred = arr => {
   let total = 0;
   for(let i = arr.length - 1; i >= 0; i--){
@@ -44,8 +45,9 @@ console.log(validateCred(valid1));
 console.log(validateCred(invalid1));
 */
 
-console.log(validateCred(myCard));
+console.log(validateCred(myCard));  // Logs true or false depending on if the myCard variable is a valid card
 
+// Creates an array of invalid cards when an array of nested arrays are ran inside the function
 const findInvalidCards = cards => {
   const invalidCards = [];
   for(let i = 0; i < cards.length; i++){
@@ -57,8 +59,9 @@ const findInvalidCards = cards => {
   return invalidCards;
 };
 
-//console.log(findInvalidCards(batch));
+// console.log(findInvalidCards(batch));
 
+// Finds the company of the card issuer
 const idInvalidCardCompanies = invalidNums => {
   const companies = [];
   for (let i = 0; i < invalidNums.length; i++) {
